e74adef2d4f64b7270053cdd63810abfadb131c9
AKozhemyako <admin_tp@ukr.net>
2018-05-27 20:34:51 +0300

    Add action show and create to articles_controller.rb file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def index
   end
 
@@ -6,5 +10,15 @@ class ArticlesController < ApplicationController
   end
 
   def create
+  	@article = Article.new(params[:article])
+ 
+  @article.save
+  redirect_to @article
+  end
+  
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
